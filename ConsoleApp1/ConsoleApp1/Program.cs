81cdee6188385986566f6e5d950a2a924582debc
Express01 <115688744+Express01@users.noreply.github.com>
2023-02-09 12:20:02 +0300

    разработка упрощенной игры BlackJack

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+bool isPlayerContinue = true;
+bool isComputerContinue = true;
+Random random = new Random();
+do
+{
+Console.WriteLine("Будешь тянуть карту?(y/n)");
+string answer = Console.ReadLine();
+
+if (answer=="y")
+{
+    Console.WriteLine("Вы тяните карту");
+}
+if (answer=="n")
+{
+    Console.WriteLine("Вы пассуете");
+        isPlayerContinue = false;
+}
+    int decisionNumber = 0;
+    if (isComputerContinue)
+    {
+
+      decisionNumber = random.Next(0,2);
+
+    }
+    if (decisionNumber==0)
+    {
+        Console.WriteLine("Компьютер тянут карту");
+    }
+    if (decisionNumber==1)
+    {
+        Console.WriteLine("Компьютер пассует");
+        isComputerContinue = false;
+    }
+} while (isPlayerContinue||isComputerContinue);
+
