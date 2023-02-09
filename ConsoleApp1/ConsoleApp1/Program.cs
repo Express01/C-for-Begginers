57f9ad757b6810f6b2264b9c4727f6fbd5537f3e
Express01 <115688744+Express01@users.noreply.github.com>
2023-02-09 12:22:03 +0300

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//int[] cards = new int[]
+//{
+//    2,3,4,5,6,7,8,9,10,       //цифры
+//    10,10,10,                //картинки
+//    11 ,                      //туз
+//      2,3,4,5,6,7,8,9,10,       //цифры
+//    10,10,10,                //картинки
+//    11  ,                     //туз
+//      2,3,4,5,6,7,8,9,10,       //цифры
+//    10,10,10,                //картинки
+//    11 ,                    //туз
+//      2,3,4,5,6,7,8,9,10,       //цифры
+//    10,10,10,                //картинки
+//    11                       //туз
+//};
+int[] cards = new int[] { 1, 2, 3, 4 };
+cards = Shuffle(cards);
+cards = Shuffle(cards);
+cards = Shuffle(cards);
+
+int[] Shuffle(int[] cards)
+{
+    int[] result = new int[cards.Length];
+    Random random1 = new Random();
+    random1.Next(0, cards.Length);
+    for (int i = 0; i < cards.Length; i++)
+    {
+
+    }
+    return result;
+}
+
 bool isPlayerContinue = true;
 bool isComputerContinue = true;
 Random random = new Random();
+int decisionNumber = 0;
 do
 {
 Console.WriteLine("Будешь тянуть карту?(y/n)");
@@ -16,7 +49,7 @@ if (answer=="n")
     Console.WriteLine("Вы пассуете");
         isPlayerContinue = false;
 }
-    int decisionNumber = 0;
+   
     if (isComputerContinue)
     {
 
